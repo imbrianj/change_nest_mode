b4a669e7ff5b7ac1b13a0b7215c13603d62c5d2f
imbrianj <bjohnsonwsu@gmail.com>
2013-09-25 10:57:42 -0700

    initial commit

---
+++ change_nest_mode.groovy
@@ -0,0 +1,40 @@
+/**
+ *  Change Nest Mode
+ *
+ *  Author: brian@bevey.org
+ *  Date: 9/25/13
+ *
+ *  Simply marks any thermostat "away" if able (primarily focused on the Nest
+ *  thermostat).  This is intended to be used with an "Away" or "Home" mode.
+ */
+
+preferences {
+  section("Change to this mode to...") {
+    input "newMode", "mode", metadata:[values:["Away","Home"]]
+  }
+
+  section("Change these thermostats modes...") {
+    input "thermostats", "capability.thermostat", multiple: true
+  }
+}
+
+def installed() {
+	subscribe(location, changeMode)
+	subscribe(app, changeMode)
+}
+
+def updated() {
+	unsubscribe()
+	subscribe(location, changeMode)
+	subscribe(app, changeMode)
+}
+
+def changeMode(evt) {
+  if(newMode == "Away") {
+    thermostats?.away()
+  }
+
+  else {
+    thermostats?.present()
+  }
+}
